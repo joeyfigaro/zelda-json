475d0166bbeb786d979bb4ab1f4b1f29d3bbfe6f
Joey Figaro <joey@joeyfigaro.com>
2017-03-26 20:41:49 -0400

    added flattening utils; fixed exports up

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const elixirs = {
-	require('./elixirs/haste.json'),
-	require('./elixirs/max-stamina.json'),
-	require('./elixirs/resistances.json'),
-	require('./elixirs/stamina.json')
-};
+const scalar = require('./lib/utils').scalar;
+const flatten = require('./lib/utils').flatten;
 
-const recipes = [
-	require('./recipes/haste.json'),
-	require('./recipes/defense.json'),
-	require('./recipes/max-hearts.json'),
-	require('./recipes/max-stamina.json'),
-	require('./recipes/resistances.json'),
-	require('./recipes/stealth.json')
+module.exports = {
+	elixirs: flatten([
+		require('./elixirs/haste.json'),
+		require('./elixirs/max-stamina.json'),
+		require('./elixirs/resistances.json'),
+		require('./elixirs/stamina.json')
+	]),
+	recipes: flatten([
+		require('./recipes/haste.json'),
+		require('./recipes/defense.json'),
+		require('./recipes/max-hearts.json'),
+		require('./recipes/max-stamina.json'),
+		require('./recipes/resistances.json'),
+		require('./recipes/stealth.json')
+	])
 };
-
-module.exports = { elixirs: elixirs, recipes: recipes };
