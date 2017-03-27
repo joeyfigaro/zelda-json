9b287310c7e2bd1b9d4cb362dae4e551740f92a0
Joey Figaro <joey@joeyfigaro.com>
2017-03-26 20:25:20 -0400

    updated exports for package

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const elixirs = {
-	haste: require('./elixirs/haste.json'),
-	'max-stamina': require('./elixirs/max-stamina.json'),
-	resistances: require('./elixirs/resistances.json'),
-	stamina: require('./elixirs/stamina.json')
+	require('./elixirs/haste.json'),
+	require('./elixirs/max-stamina.json'),
+	require('./elixirs/resistances.json'),
+	require('./elixirs/stamina.json')
 };
 
-const recipes = {
-	haste: require('./recipes/haste.json'),
-	defense: require('./recipes/defense.json'),
-	'max-hearts': require('./recipes/max-hearts.json'),
-	'max-stamina': require('./recipes/max-stamina.json'),
-	resistances: require('./recipes/resistances.json'),
-	stealth: require('./recipes/stealth.json')
+const recipes = [
+	require('./recipes/haste.json'),
+	require('./recipes/defense.json'),
+	require('./recipes/max-hearts.json'),
+	require('./recipes/max-stamina.json'),
+	require('./recipes/resistances.json'),
+	require('./recipes/stealth.json')
 };
 
 module.exports = { elixirs: elixirs, recipes: recipes };
