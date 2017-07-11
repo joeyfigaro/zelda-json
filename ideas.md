bc889d31e6bb54e15a12c2ec241101bb5b28254d
Joey Figaro <joeyfigaro@Joeys-MBP-2.fios-router.home>
2017-07-11 17:54:24 -0400

    added boilerplate for materials updates / added ideas notes

---
+++ ideas.md
@@ -0,0 +1,9 @@
+# Critical Cooking Effects
+- Detail critical results for all elixirs/recipes
+
+# Farming Data
+- Collect youtube videos for tips
+- Add location information for ingredients
+
+# Media/images
+- Add images for materials/recipes/elixirs
---
+++ materials/rare.js
@@ -0,0 +1,15 @@
+[
+	{
+		"name": "Star Fragment",
+		"value": 300,
+		"hearts": 0,
+		"category": "",
+		"duration": 30,
+		"availability": "Shooting stars",
+		"meta": {
+			videos: [
+				"https://www.youtube.com/watch?v=cUlNsoV2oHI"
+			]
+		}
+	}
+]
